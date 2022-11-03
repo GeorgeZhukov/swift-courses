c4cac0a123bdc137e842509634291a94185dc05d
George <george.zhukov@shiphawk.com>
2022-11-03 20:33:27 +0200

    Merge tasks

--- Tests/SwiftCoursesTests/mainTests.swift
+++ Tests/SwiftCoursesTests/mainTests.swift
@@ -2,14 +2,27 @@ import XCTest
 @testable import SwiftCourses
 
 final class mainTests: XCTestCase {
+    private var buffer: String = ""
+
+    override func setUp() {
+        
+        buffer = SwiftCourses().text
+    }
+
+
     func testMain() throws {
-        XCTAssertNotNil(SwiftCourses().text)
+        XCTAssertNotNil(buffer)
     }
     
-    // func testRendersHometask1() throws {
-    //     XCTAssertTrue(SwiftCourses().text.contains("Hometask #1"))
-    // }
-    // func tastHometask2() throws {
-    //     XCTAssertTrue(SwiftCourses().text.contains("Hometask #2"))
-    // }
+    func testRendersHometask1() throws {
+        XCTAssertTrue(buffer.contains("Hometask #1"))
+    }
+
+    func tastHometask2() throws {
+        XCTAssertTrue(buffer.contains("Hometask #2"))
+    }
+
+    func tastHometask3() throws {
+        XCTAssertTrue(buffer.contains("Hometask #3"))
+    }
 }
